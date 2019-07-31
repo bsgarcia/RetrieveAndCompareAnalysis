0934f117218d95547729ba6557d1939669734871
getzneet <basilegarcia@gmail.com>
2019-07-31 18:43:35 +0200

    update

--- main.m
+++ main.m
@@ -1,10 +1,12 @@
 clear all
 %------------------------------------------------------------------------
-data = load('partialdata');
-data = data.learningdatarandc1(:, :);
+data = load('data/first_88');
+data = data.learningdatarandc88(:, :);
 
 %----------------------------------
 %sem = @(x) std(x)./sqrt(size(data,2));
+
+
 
 % get parameters
 %------------------------------------------------------------------------
@@ -37,19 +39,40 @@ idx.dist = 28;
 idx.plot = 29;
 idx.cont1 = 14;
 idx.cont2 = 15;
+%idx.prolific = 2;
 %------------------------------------------------------------------------
 
 corr_catch = extract_catch_trials(data, sub_ids, idx);
-[cho1, out1, corr1, rew] = extract_learning_data(...
+[cho1, out1, corr1, con1, rew] = extract_learning_data(...
     data, ncond, nsession, sub_ids, idx);
-[corr, cho, out, p1, p2, ev1, ev2, ctch, cont1, cont2] = extract_elicitation_data(...
+[corr, cho, out2, p1, p2, ev1, ev2, ctch, cont1, cont2] = extract_elicitation_data(...
     data, sub_ids, idx, 0);
 
+% Split depending on optimism tendency
+% -----------------------------------------------------------------------
+data = load('data/fit/online_exp');
+parameters = data.data('parameters');
+delta_alpha = parameters(:, 2, 2) - parameters(:, 3, 2);
+[sorted, idx_order] = sort(delta_alpha);
+cho = cho(idx_order, :);
+p2 = p2(idx_order, :);
+cont1 = cont1(idx_order, :);
 
-%figure
-%bar(mean(corr{:, 1}));
-%errorbar(mean(corr{:, 1}), std(corr{:, 1}));
+psym = zeros(4, 2);
+for con = 1:4
+    for c = 1:2
+        temp = out1(logical((con1 == con) .* (cho1 == c))) == 1;
+        psym(con , c) = mean(temp);
+    end
+end
 
+figure
+bar(psym);
+ylabel('P(outcome=1)')
+xlabel('Conditions')
+legend('Option 1', 'Option 2')
+ylim([0, 1.0]);
+    
 %------------------------------------------------------------------------
 % Compute corr choice rate
 %------------------------------------------------------------------------
@@ -58,7 +81,8 @@ corr_rate = zeros(size(corr1, 1), 30, 4);
 for sub = 1:size(corr1, 1)
     for t = 1:30
         for j = 1:4
-            corr_rate(sub, t, j) = mean(corr1(sub, 1:t, j) == 1);
+            d = corr1(sub, con1(sub, :) == j);
+            corr_rate(sub, t, j) = mean(d(1:t));
         end
     end
 end
@@ -67,26 +91,28 @@ end
 % PLOT
 %------------------------------------------------------------------------
 %i = 1;
-titles = {'-0.8 vs 08', '-0.6 vs 0.6', '-0.4 vs 0.4', '-0.2 vs 0.2'};
+titles = {'-0.8 vs 0.8', '-0.6 vs 0.6', '-0.4 vs 0.4', '-0.2 vs 0.2'};
 figure;
 for cond = 1:4
     subplot(1, 4, cond)
-    options.error = 'sem';
-    options.handle = 1;
-    options.color_area = [0.4660    0.6740    0.1880];
-    options.alpha = 0.4;
-    options.color_line = [0.4660    0.6740    0.1880];
-    options.line_width = 0.8;
 
-    %options.handle = cond;
-    %i = i + 1;
-    plot_areaerrorbar(...
-            corr_rate(:, :, cond), options...
+    reversalplot(...
+        corr_rate(:, :, cond)',...
+        [],...
+        [],...
+        ones(3) * 0.5,...
+        [0.4660    0.6740    0.1880],...
+        1,...
+        0.38,...
+        -0.01,...
+        1.01,...
+        15,...
+        titles{cond},...
+        'trials',...
+        'correct choice rate' ...
     );
-    xlabel('trials');
-    ylabel('correct choice rate');
-    title(titles{cond});
-    ylim([-0.01, 1.01])
+
+    i = i + 1;
 end
 
 
@@ -100,63 +126,120 @@ for i = 1:size(corr, 1)
     for j = 1:length(pcue)
         for k = 1:length(cont)
             temp = cho(i, logical((p2(i, :) == pcue(j)) .* (cont1(i, :) == cont(k))));
+            disp(temp);
             plearn(i, j, k) = temp == 1;
         end
     end
 end
 
 
-X = repmat(pcue, size(cho, 1), 1);
-pp = zeros(length(cont), length(pcue));
-for i = 1:length(cont)
-    Y = plearn(:, :, i);
-    disp(i);
-    %     [B,dev,stats] = mnrfit(X, Y);
-    %     pp(i, :) = mnrval(B, plearn(:, :, i));
-    [logitCoef,dev] = glmfit(pcue', mean(plearn(:, :, i), 1)','binomial','logit');
-    pp(i, :) = glmval(logitCoef,pcue','logit');
+titles = {'Low \Delta\alpha', 'High \Delta\alpha'};
+tt = 0;
+for k = {1:45, 46:90}
+    tt = tt + 1;
+    k = k{:};
+    prop = zeros(length(cont), length(pcue));
+    for j = 1:length(pcue)
+        for l = 1:length(cont)
+           temp1 = cho(k, :);
+           temp = temp1(logical((p2(k, :) == pcue(j)) .* (cont1(k, :) == cont(l))));
+           prop(l, j) = mean(temp == 1);
+       end
+    end
+   
+    X = repmat(pcue, 45, 1);
+    pp = zeros(length(cont), length(pcue));
+    for i = 1:length(cont)
+        Y = plearn(k, :, i);
+        disp(i);
+        %     [B,dev,stats] = mnrfit(X, Y);
+        %     pp(i, :) = mnrval(B, plearn(:, :, i));
+        [logitCoef,dev] = glmfit(...
+            reshape(X, [], 1), reshape(plearn(k, :, i), [], 1), 'binomial','logit');
+        pp(i, :) = glmval(logitCoef,pcue','logit');
+    end
+
+
+    figure
+    pwin = [0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9];
+    suptitle(titles{tt});
+
+    for i = 1:length(cont)
+        subplot(4, 2, i)
+        lin = plot(...
+            pcue,  pp(i, :),... %'bs', pcue, pp(i, :),  'b-', 'MarkerEdgeColor',...
+            'Color', [0.4660    0.6740    0.1880] ...
+            );
+        ind_point = interp1(lin.YData, lin.XData, 0.5);
+        if mod(i, 2) ~= 0
+            ylabel('P(choose learnt value)');
+        end
+        if ismember(i, [7, 8])
+            xlabel('Described cue win probability');
+        end
+        hold on
+        scatter(pcue, prop(i, :), 'MarkerEdgeColor', [0.4660    0.6740    0.1880]);
+        scatter(ind_point, 0.5, 'MarkerFaceColor', 'r');
+
+        plot(ones(10)*pwin(i), linspace(0.1, 0.9, 10), 'LineStyle', '--', 'Color', [0, 0, 0], 'LineWidth', 0.6);
+        %[xi,yi] = polyxpoly(ones(10)*pwin(i),linspace(0.1, 0.9, 10),pcue,  pp(i, :));
+        %scatter(xi, yi, 'Color', 'r');
+        if i < 6
+        text(pwin(i)+0.03, 0.8, sprintf('P(win) = %0.1f', pwin(i)), 'FontSize', 7);
+        else
+
+            text(pwin(i)-0.30, 0.8, sprintf('P(win) = %0.1f', pwin(i)), 'FontSize', 7);
+        end
+
+        plot(linspace(0, 1, 12), ones(12)*0.5, 'LineStyle', ':', 'Color', [0, 0, 0]);
+        ylim([-0.08, 1.08]);
+        xlim([-0.08, 1.08]);   
+    end
 end
+
+[corr, cho, out2, p1, p2, ev1, ev2, ctch, cont1, cont2] = extract_elicitation_data(...
+    data, sub_ids, idx, 2);
+i = 1;
+for p = pwin
+    mn(i, :) = cho(p1(:, :) == p);
+    i = i + 1;
+end
+
 
 figure
-titles = [1:9]./10;
-
-for i = 1:length(cont)
-    subplot(4, 4, i)
-    plot(...
-        pcue,  pp(i, :), 'bs', pcue, pp(i, :),  'b-', 'MarkerEdgeColor',...
-        [0.4660    0.6740    0.1880], 'Color', [0.4660    0.6740    0.1880] ...
-        );
-    ylabel('P(choose learnt value)');
-    xlabel('Described cue win probability');
-    title(sprintf('%0.1f', titles(i)));
-    ylim([-0.08, 1.08]);
-    xlim([-0.08, 1.08]);
-    
-end
+pirateplot(...
+    mn, rand(8, 3),...
+    -0.8, 100.8, 20, 'Slider choices' , 'P(win of learnt value)',...
+    'Choice (% of odds reward is +1)', pwin)
 
 %set(ha(1:4),'XTickLabel',''); set(ha,'YTickLabel','')
 
-function [cho, out, corr, rew] = extract_learning_data(data, ncond, nsession, sub_ids, idx)
+function [cho, out, corr, con, rew] = extract_learning_data(data, ncond, nsession, sub_ids, idx)
 i = 1;
 for id = 1:length(sub_ids)
     sub = sub_ids(id);
     mask_sub = data(:,1) == sub;
     if ismember(sum(data(:, 1) == sub), [255, 285])
-        for cond = 0:ncond
-            mask_cond = data(:, idx.cond) == cond;
+            %mask_cond = data(:, idx.cond) == cond;
             mask_sess = ismember(data(:, idx.sess), [0]);
-            mask = logical(mask_sub .* mask_cond .* mask_sess);
+            mask_eli = data(:, idx.elic) == -1;
+            mask = logical(mask_sub .* mask_sess .* mask_eli);
             [noneed, trialorder] = sort(data(mask, idx.trial_idx));
             tempcho = data(mask, idx.cho);
-            cho(i, 1:30, cond+1) = tempcho(trialorder);
+            cho(i, :) = tempcho(trialorder);
             tempout = data(mask, idx.out);
-            out(i, 1:30, cond+1) = tempout(trialorder);
+            out(i, :) = tempout(trialorder);
             tempcorr = data(mask, idx.corr);
-            corr(i, 1:30, cond+1) = tempcorr(trialorder);
+            corr(i, :) = tempcorr(trialorder);
             temprew = data(mask, idx.rew);
-            rew(i, 1:30, cond+1) = temprew(trialorder);
-        end
-        i = i+1;
+            rew(i, :) = temprew(trialorder);
+            tempcon = data(mask, idx.cond);
+            con(i, :) = tempcon(trialorder) + 1;
+%             temp_prolid = str2num(data(mask, idx.prolific));
+%             prolid(i, :) = temp_prolid(trialorder);
+%         if sum(corr(i, :)) > 90
+             i = i+1;
+%         end
     end
 end
 end
@@ -230,7 +313,11 @@ for id = 1:length(sub_ids)
         temp_p2 = data(mask, idx.p2);
         p2(i, :) = temp_p2(trialorder);
         
-        i = i + 1;
+%         temp_prolid = str2num(data(mask, idx.prolific));
+%         prolid(i, :) = temp_prolid(trialorder);
+%         if sum(corr(i, :)) > 40
+         i = i + 1;
+%         end
         
     end
 end
